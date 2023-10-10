1820a16de82f3b9898246906bb3167b68366c917
Toygun <Dehjudith21@gmail.com>
2023-10-09 19:20:21 -0700

    betty code finally followed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+
+/* main - creating a program using the puts function instead of printf */
+
+
 int main(void)
+
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
