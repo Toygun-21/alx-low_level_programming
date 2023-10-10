992c2082eeaba40da061558a97ddcbeee7c76497
Toygun <Dehjudith21@gmail.com>
2023-10-09 19:24:18 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/* main - creating a program using the puts function instead of printf */
-
+/**
+ * main - A program that prints a line with puts function
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
