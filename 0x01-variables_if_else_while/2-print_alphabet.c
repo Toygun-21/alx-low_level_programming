7da28eb23ef5837bb3095f2ab60a19fc5bd6ea0e
Toygun <Dehjudith21@gmail.com>
2023-10-13 09:53:00 -0700

    2-print_alphabet done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ /**
+  * main- Entry point
+  *
+  * Always: Return 0
+  */
+
+int main(void)
+{ 
+	char letter = 'a';
+	
+	while(letter <= 'z')
+	{
+	  putchar(letter);('\n');
+	  letter++;
+	}
+	
+	putchar('\n');	
+
+	return (0);
+}
