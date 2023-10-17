ad52e0d485d88bdda144f422265a05af453b48e7
toygun_21 <dehjudith21@gmail.com>
2023-10-17 20:27:47 +0000

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+	for(print_alphabet="a";print_alphabet <= "z"; print_alphabet++)
+	{
+		_putchar(print_alphabet);
+	}
+	_putchar('\n');
+    return (0);
+}
