eddd430870f2dd5af33883952ae42a68edb3961c
toygun_21 <dehjudith21@gmail.com>
2023-10-19 19:16:50 +0000

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* _isdigit - digits 0 -9
+* _isdigit - defines gits from  0 -9
 * @c - integer to check
-* Return: 0 or 1
-**/
+* Return: 0 if false  or 1 if true
+*/
 int _isdidgit(int c)
 {
-if(c >= 0 && <= 9)
+if(c >= '0' && <= '9')
 return (1);
 else
 return (0);
