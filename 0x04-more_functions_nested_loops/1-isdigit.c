0c24b150088170993d1bdf56e22c90017acf4fc0
toygun_21 <dehjudith21@gmail.com>
2023-10-19 19:19:54 +0000

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,9 @@
 * @c - integer to check
 * Return: 0 if false  or 1 if true
 */
-int _isdidgit(int c)
+int _isdigit(int c)
 {
-if(c >= '0' && <= '9')
+if(c >= 48  && c <= 57)
 return (1);
 else
 return (0);
