e14ce88d0614714e8e81fd8def8614ce1d58ace4
toygun_21 <dehjudith21@gmail.com>
2023-10-19 19:24:44 +0000

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* _isdigit - defines gits from  0 -9
-* @c - integer to check
-* Return: 0 if false  or 1 if true
-*/
+ * _isdigit - defines gits from  0 -9
+ * @c - integer to check
+ * Return: 0 if false  or 1 if true
+ */
 int _isdigit(int c)
 {
 if(c >= 48  && c <= 57)
