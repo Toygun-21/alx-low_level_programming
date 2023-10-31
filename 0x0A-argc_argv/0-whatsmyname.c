80db7c1bbe62e24f948a6dd8332b3cbd5dd1d39b
toygun_21 <dehjudith21@gmail.com>
2023-10-31 11:08:38 +0000

    project done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Code Description: Prints the name of the program
+ * @argc: No of command line arguments
+ * @argv: Array name
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
