5d8b6fdcb520735ba6f942fc882e3db45981c8b6
toygun_21 <dehjudith21@gmail.com>
2023-10-31 11:10:58 +0000

    done

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memcpy - Entry point
+ * Description: Copies memory area
+ * @dest: Character
+ * @src: Character
+ * @n: Integer
+ * Return: char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *ans = dest;
+while (n--)
+{
+*dest++ = *src++;
+}
+return (ans);
+}
